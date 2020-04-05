c326108a648801e42af63e1fe2032f38501a22dd
Johnny <j_chen187@yahoo.com>
2020-04-05 16:37:59 -0400

    Add next story for Button

--- basenextjs/stories/1-Button.stories.js
+++ basenextjs/stories/1-Button.stories.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
+import Prism from 'prismjs';
+
+import ButtonWithSCSS from '../src/components/Button/ButtonWithSCSS';
+// import ButtonWithCSS from "../src/components/Button/ButtonWithCSS";
+import { SearchResult } from '../src/components/SearchResult';
+
+import '../src/styles/prism.css';
 
 export default {
   title: 'Button',
@@ -15,4 +22,34 @@ export const Emoji = () => (
       😀 😎 👍 💯
     </span>
   </Button>
+);
+
+export const PrismEx = () => (
+  <div>
+    <pre className="asdasd">
+      <code className="language-javascript">
+        var data = 12
+      </code>
+    </pre>
+    <pre className="asdasd">
+      <code className="">
+        var data = 12
+      </code>
+    </pre>
+    -----
+    {/*
+    */}
+    <p className="orange">Using Regular CSS</p>
+    <p className="blue">Using Regular SCSS</p>
+    -----
+    <ButtonWithSCSS />
+    {/*
+    <ButtonWithCSS />
+    */}
+    <SearchResult
+      question="what is this"
+      answer="the answer"
+      code="some code"
+    />
+  </div>
 );
