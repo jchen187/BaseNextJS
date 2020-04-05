6e1ba63b8834f2ae3646ad520bf4555d413ea05a
Johnny <j_chen187@yahoo.com>
2020-04-05 15:57:43 -0400

    Eslint all the files in pages

--- basenextjs/src/pages/Explain/index.js
+++ basenextjs/src/pages/Explain/index.js
@@ -1,108 +1,71 @@
 import React from 'react';
 import Head from 'next/head';
-import Prism from 'prismjs';
 
-// not default export, so need the {} and the exact name
-// make sense if you are exporting a lot of items. how do you differentiate
-// import { ButtonWithCSS } from '../components/Button/ButtonWithCSS';
-import ButtonWithCSS from '../components/Button/ButtonWithCSS';
-import ButtonWithSCSS from '../components/Button/ButtonWithSCSS';
-import { SearchResult } from '../components/SearchResult';
+function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-const code = 'var data = 1;';
+      <main>
+        <h1 className="title">
+          Welcome to
+          {' '}
+          <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <p className="description">
+          Get started by editing
+          {' '}
+          <code>pages/index.js</code>
+        </p>
 
-// Returns a highlighted HTML string
-const html = Prism.highlight(code, Prism.languages.javascript, 'javascript');
-
-class Home extends React.Component {
-  componentDidMount() {
-    Prism.highlightAll(); // need this to properly color
-    console.log('Prism work');
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <main>
-          <h1 className="title">
-            Welcome to
-            {' '}
-            <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-
-          CSS
-          <ButtonWithCSS />
-          --------------------
-          SCSS and Some Code
-          <ButtonWithSCSS />
-
-          --------------------
-          <pre>
-            <code dangerouslySetInnerHTML={{ __html: html }} />
-          </pre>
-          <pre className="language-javascript">Espero que tu recuperes</pre>
-
-          <SearchResult
-            question="what is this"
-            answer="the answer"
-            code="some code"
-          />
-          <p className="description">
-            Get started by editing
-            {' '}
-            <code>pages/index.js</code>
-          </p>
-
-          <div className="grid">
-            <a href="https://nextjs.org/docs" className="card">
-              <h3>Documentation &rarr;</h3>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className="card">
-              <h3>Learn &rarr;</h3>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/zeit/next.js/tree/master/examples"
-              className="card"
-            >
-              <h3>Examples &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className="card"
-            >
-              <h3>Deploy &rarr;</h3>
-              <p>
-                Instantly deploy your Next.js site to a public URL with ZEIT Now.
-              </p>
-            </a>
-          </div>
-        </main>
-
-        <footer>
-          <a
-            href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by
-            {' '}
-            <img src="/zeit.svg" alt="ZEIT Logo" />
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
           </a>
-        </footer>
 
-        <style jsx>
-          {`
+          <a href="https://nextjs.org/learn" className="card">
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/zeit/next.js/tree/master/examples"
+            className="card"
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="card"
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with ZEIT Now.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer>
+        <a
+          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by
+          {' '}
+          <img src="/zeit.svg" alt="ZEIT Logo" />
+        </a>
+      </footer>
+
+      <style jsx>
+        {`
             .container {
               min-height: 100vh;
               padding: 0 0.5rem;
@@ -228,10 +191,10 @@ class Home extends React.Component {
               }
             }
           `}
-        </style>
+      </style>
 
-        <style jsx global>
-          {`
+      <style jsx global>
+        {`
             html,
             body {
               padding: 0;
@@ -244,9 +207,8 @@ class Home extends React.Component {
               box-sizing: border-box;
             }
           `}
-        </style>
-      </div>
-    );
-  }
+      </style>
+    </div>
+  );
 }
 export default Home;
--- basenextjs/src/pages/_app.js
+++ basenextjs/src/pages/_app.js
@@ -1,9 +1,17 @@
-//import '../styles.css'
-import '../styles/prism.css'
-import '../styles/global.scss'
+/* eslint-disable
+ react/jsx-props-no-spreading,
+ react/prop-types,
+ react/require-default-props,
+ react/forbid-prop-types
+*/
+
+import React from 'react';
+
+import '../styles/prism.css';
+import '../styles/global.scss';
 // import "prismjs/themes/prism.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
