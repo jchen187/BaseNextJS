2224b9b4fc2d3b2b588d1d4e34f5147f74a275c7
Johnny <j_chen187@yahoo.com>
2020-04-10 23:32:09 -0400

    Add prism override styles for the buttons in the toolbar.

--- basenextjs/src/javascript/prismPluginsCSS.js
+++ basenextjs/src/javascript/prismPluginsCSS.js
@@ -5,3 +5,5 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'prismjs/plugins/command-line/prism-command-line.css';
 // import 'prismjs/plugins/previewers/prism-previewers.css';
 // import 'prismjs/plugins/match-braces/prism-match-braces.css';
+
+import '../styles/prism-override.css';
