2cc7fbf174a06efda2bd1bb8ed0c1d6830ce6838
Johnny <j_chen187@yahoo.com>
2020-04-10 22:54:41 -0400

    Import the plugins from the page you are using itself because you will run into issues when you try to build and run on production

--- basenextjs/src/pages/_app.js
+++ basenextjs/src/pages/_app.js
@@ -25,18 +25,6 @@ import 'prismjs/plugins/command-line/prism-command-line.css';
 // import 'prismjs/plugins/previewers/prism-previewers.css';
 // import 'prismjs/plugins/match-braces/prism-match-braces.css';
 
-// Prism Plugins
-// You need the toolbar to get the show-lang, copy, and command line to work
-import 'prismjs/plugins/toolbar/prism-toolbar';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
-import 'prismjs/plugins/show-language/prism-show-language';
-import 'prismjs/plugins/download-button/prism-download-button';
-import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
-import 'prismjs/plugins/command-line/prism-command-line';
-import 'prismjs/plugins/previewers/prism-previewers';
-import 'prismjs/plugins/match-braces/prism-match-braces';
-
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
---
+++ basenextjs/src/prismPlugins.js
@@ -0,0 +1,13 @@
+// Prism Plugins
+// You need the toolbar to get the show-lang, copy, and command line to work
+
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import 'prismjs/plugins/show-language/prism-show-language';
+// import 'prismjs/plugins/download-button/prism-download-button';
+// import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/command-line/prism-command-line';
+// import 'prismjs/plugins/previewers/prism-previewers';
+// import 'prismjs/plugins/match-braces/prism-match-braces';
+
