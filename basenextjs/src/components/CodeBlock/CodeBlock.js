17bf84d677c55e5f15adb33b647790d546efc7ec
Johnny <j_chen187@yahoo.com>
2020-04-10 22:48:11 -0400

    Update CodeBlock to work properly with command line plugin

--- basenextjs/src/components/CodeBlock/CodeBlock.js
+++ basenextjs/src/components/CodeBlock/CodeBlock.js
@@ -23,25 +23,46 @@ class CodeBlock extends React.Component {
   }
 
   render() {
-    const { code, plugins, language } = this.props;
+    const {
+      code,
+      children,
+      dataUser,
+      dataHost,
+      dataOutput,
+      plugins,
+      language
+    } = this.props;
+
     return (
-      <pre className={!plugins ? '' : plugins.join(' ')}>
+      <>
+      <pre className={!plugins ? '' : plugins.join(' ')}
+        data-user={dataUser}
+        data-host={dataHost}
+        data-output={dataOutput}
+      >
         <code ref={this.ref} className={`language-${language}`}>
           {code.trim()}
         </code>
       </pre>
+      </>
     );
   }
 }
 
 CodeBlock.propTypes = {
   code: PropTypes.string,
+  dataUser: PropTypes.string,
+  dataHost: PropTypes.string,
+  dataOutput: PropTypes.string,
   language: PropTypes.string,
   plugins: PropTypes.arrayOf(PropTypes.string),
 };
 
 CodeBlock.defaultProps = {
   code: '',
+  dataUser: '',
+  dataHost: '',
+  dataOutput: '',
   language: 'javascript',
   plugins: null,
 };
