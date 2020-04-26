460028f41f0f87a88c366dc0f56143d97930d57b
Johnny <j_chen187@yahoo.com>
2020-04-26 13:10:01 -0400

    Copy yeoman files from previous project

---
+++ generator-base/app/templates/storybook.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+
+import <%= componentName %> from '../components/<%= componentName %>';
+import globalStyles from './styles.scss';
+
+const styles = {
+  textAlign: 'center',
+};
+const CenterDecorator = storyFn => <div style={styles}>{storyFn()}</div>;
+
+storiesOf('<%= componentName %>', module)
+  .addDecorator(CenterDecorator)
+  .add('Default with Global Styles', () => <div className={globalStyles.test}><<%= componentName %> /></div>)
+  .add('Version 1', () => <<%= componentName %> />)
+  .add('Version 2', () => <<%= componentName %> />);
